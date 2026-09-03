49625856bad9c9491e4d7d2b0758312b2914e4a5
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-03 21:17:10 +0900

    Char2.java作成

---
+++ JavaSample/chapter3/Char.java
@@ -0,0 +1,8 @@
+public class Char {
+  public static void main (String[] args) {
+    System.out.println('J');
+    System.out.println('A');
+    System.out.println('V');
+    System.out.println('A');
+  }
+}
