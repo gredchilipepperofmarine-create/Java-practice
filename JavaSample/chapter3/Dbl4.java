26b2a677b026a64fb636bd0c2d47143abecb8103
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-03 20:28:51 +0900

    Dbl4.java作成

---
+++ JavaSample/chapter3/Dbl4.java
@@ -0,0 +1,8 @@
+public class Dbl4 {
+  public static void main (String[] args) {
+    System.out.println(5/2);
+    System.out.println(5.0/2.0);
+    System.out.println(5/2.0);
+    System.out.println(5.0/2);
+  }
+}
