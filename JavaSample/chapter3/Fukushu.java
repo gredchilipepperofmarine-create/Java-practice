d0cbeda5ea2318e1ec22cb856fe5c34716fe4258
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-09 16:01:30 +0900

    練習問題

---
+++ JavaSample/chapter3/Fukushu.java
@@ -0,0 +1,26 @@
+public class Fukushu {
+  public static void main (String[] args) {
+    System.out.println(12+3);
+    System.out.println(12-3);
+    System.out.println(12*3);
+    System.out.println(12/3);
+    System.out.println(12%3);
+    System.out.println(5/2);
+    System.out.println(5%2);
+    System.out.println(2+3*4);
+    System.out.println((2+3)*4);
+    System.out.println(2-3+4);
+    System.out.println(1.2+0.3);
+    System.out.println(1.2-0.3);
+    System.out.println(1.2*0.3);
+    System.out.println(1.2/0.3);
+    System.out.println(1.2%0.3);
+    System.out.println(5.0/2.0);
+    System.out.println(5.0/0.0);
+    System.out.println(5.0%0.0);
+    System.out.println(5/2);
+    System.out.println(5.0/2.0);
+    System.out.println(5/2.0);
+    System.out.println(5.0/2);
+  }
+}
