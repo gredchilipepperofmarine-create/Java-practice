24197fccb04a8977cdb8eadf37680997607e630e
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-09 16:11:02 +0900

    練習問題chapter3終了

--- JavaSample/chapter3/Fukushu.java
+++ JavaSample/chapter3/Fukushu.java
@@ -22,5 +22,15 @@ public class Fukushu {
     System.out.println(5.0/2.0);
     System.out.println(5/2.0);
     System.out.println(5.0/2);
+    System.out.println(Math.sqrt(2));
+    System.out.println(Math.sqrt(3));
+    System.out.println(Math.sqrt(5));
+    System.out.println(Math.random());
+    System.out.println(Math.random()*6+1);
+    System.out.println((int)'j');
+    System.out.println("total:"+(120+340+560)+"yen");
+    System.out.println("Hello\njajv");
+    System.out.println("\"JVM\"is\"Java Virtual Machine");
+
   }
 }
