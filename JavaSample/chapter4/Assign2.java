0250793629301faafd4a90521c2cc462e3b21e44
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-15 12:07:17 +0900

    Assign2.java勉強中

---
+++ JavaSample/chapter4/Assign2.java
@@ -0,0 +1,11 @@
+public class Assign2 {
+  public static void main (String[] args) {
+    var deposit = 10000, rate = 1.1;
+    System.out.println(deposit*rate);
+    deposit *= rate;
+    System.out.println(deposit);
+    for(i=0, )
+
+  }
+  
+}
