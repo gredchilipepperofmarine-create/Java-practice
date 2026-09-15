57ef91488b752ebc8e66700bda5564d9cbf10292
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-15 16:09:49 +0900

    Widening2.java作成

--- JavaSample/chapter4/Primitive.java
+++ JavaSample/chapter4/Primitive.java
@@ -2,9 +2,9 @@ public class Primitive {
   public static void main (String[] args) {
     byte a = 100;
     short b = 1000;
-    int c = 100000000;
-    long d = 100000000L;
-    long e = 100000000000L;
+    int c = 100_000_000;
+    long d = 100_000_000L;
+    long e = 100_000_000_000L;
 
     System.out.println(a);
     System.out.println(b);
