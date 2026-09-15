68fb4b71ba722c9162c35f2d29776ce5937eef29
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-15 11:18:34 +0900

    Variable2.java作成

--- JavaSample/chapter4/Variable2.java
+++ JavaSample/chapter4/Variable2.java
@@ -1,7 +1,8 @@
 public class Variable2 {
   public static void main (String[] args) {
-    int total = 450+670+790;
-    System.out.println((int) total / 2);
+    int total = 450+670+890;
+    System.out.println(total / 2);
+    System.out.println(total / 3);
   }
   
 }
