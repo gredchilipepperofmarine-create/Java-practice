b3cce931dba654baafbd8e6782029f50b95e8810
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-15 15:34:22 +0900

    Primitive..java作成

---
+++ JavaSample/chapter4/Primitive.java
@@ -0,0 +1,17 @@
+public class Primitive {
+  public static void main (String[] args) {
+    byte a = 100;
+    short b = 1000;
+    int c = 100000000;
+    long d = 100000000L;
+    long e = 100000000000L;
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+    
+  }
+  
+}
