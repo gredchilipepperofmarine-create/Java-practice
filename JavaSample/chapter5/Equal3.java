b35eed1839669dac80482ddf9cd199f59d59012f
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-18 20:57:34 +0900

    Equal3.java作成

---
+++ JavaSample/chapter5/Equal3.java
@@ -0,0 +1,8 @@
+public class Equal3 {
+  public static void main (String[] args) {
+    String x = "pineapple", y = "pine", z = "apple";
+    System.out.println(x.equals(y+z));
+    System.out.println(x == y+z);
+  }
+  
+}
