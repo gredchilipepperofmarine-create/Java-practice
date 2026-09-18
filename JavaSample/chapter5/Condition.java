2bd9b7ed30ed5d5ed3bc4d1bba7ee50e344a1ad1
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-18 21:08:49 +0900

    Condition.java作成

---
+++ JavaSample/chapter5/Condition.java
@@ -0,0 +1,8 @@
+public class Condition {
+  public static void main (String[] args) {
+    int x = 11, y = 22;
+    System.out.println(x < 17 && x >= 9);
+    System.out.println(y < 17 && y >= 9);
+  }
+  
+}
