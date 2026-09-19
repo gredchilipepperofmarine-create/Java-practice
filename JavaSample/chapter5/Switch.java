8495f806681d0fc2ed8695033e89ea46951dc724
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-19 16:20:58 +0900

    Switch.java作成

---
+++ JavaSample/chapter5/Switch.java
@@ -0,0 +1,17 @@
+public class Switch {
+  public static void main (String[] args) {
+    int drink = 5;
+    switch (drink) {
+      case 1 :
+        System.out.println("coffee");
+        break;
+      case 2 :
+        System.out.println("tea");
+        break;
+      default :
+        System.out.println("water");
+        break;
+    }
+  }
+  
+}
