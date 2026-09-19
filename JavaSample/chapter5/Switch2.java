e67f126fd7920e6b298704893cdc58cedda23843
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-19 16:26:12 +0900

    Switch2.java作成

---
+++ JavaSample/chapter5/Switch2.java
@@ -0,0 +1,15 @@
+public class Switch2 {
+  public static void main (String[] args) {
+    int menu = 1;
+    switch (menu) {
+      case 2:
+        System.out.println("icecream");
+      case 1:
+        System.out.println("salad");
+      default :
+        System.out.println("steak");
+        break;
+    }
+  }
+  
+}
