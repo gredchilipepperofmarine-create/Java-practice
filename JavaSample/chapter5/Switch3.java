0f7eb4df6fd7c9544af33717ce6237f6553184fc
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-19 17:00:20 +0900

    Switch3.java作成

---
+++ JavaSample/chapter5/Switch3.java
@@ -0,0 +1,11 @@
+public class Switch3 {
+  public static void main (String[] args) {
+    int drink = 10;
+    switch (drink) {
+      case 1-> System.out.println("coffee");
+      case 2 -> System.out.println("tea");
+      default -> System.out.println("water");
+    }
+  }
+  
+}
