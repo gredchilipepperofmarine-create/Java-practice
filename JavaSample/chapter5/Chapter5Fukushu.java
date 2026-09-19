5a27eb077a4a6d6149e8b63f92a658fb3962881a
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-19 21:03:42 +0900

    Chapter5Fukushu調整中

---
+++ JavaSample/chapter5/Chapter5Fukushu.java
@@ -0,0 +1,7 @@
+public class Chapter5Fukushu {
+  public static void main (String[] args) {
+    int t = 24;
+    System.out.println(t>30 ? "cooler" : "off");
+  }
+  
+}
---
+++ JavaSample/chapter5/SwitchExpr2.java
@@ -0,0 +1,13 @@
+public class SwitchExpr2 {
+  public static void main (String[] args) {
+    int month = 6;
+    System.out.println(
+      switch (month) {
+        case 2 -> 28;
+        case 4, 6, 9, 11 -> 30;
+        default -> 31;
+      }
+    );
+  }
+  
+}
