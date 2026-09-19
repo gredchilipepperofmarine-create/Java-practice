531d08037490e6d5b9533ff58bbbf2fb9197d636
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-19 16:03:42 +0900

    Elseif.java作成

---
+++ JavaSample/chapter5/Elseif.java
@@ -0,0 +1,13 @@
+public class Elseif {
+  public static void main (String[] args) {
+    int t = 31;
+    if (t>30) {
+      System.out.println("cooler");
+    } else if (t>20) {
+      System.out.println("heater");
+    } else {
+      System.out.println("off");
+    }
+  }
+  
+}
