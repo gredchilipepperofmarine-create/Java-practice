09dc03667df4810c03d2dc4e10287f532f242976
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-20 13:34:59 +0900

    ForWhile.java作成

---
+++ JavaSample/chapter6/ForWhile.java
@@ -0,0 +1,11 @@
+public class ForWhile {
+  public static void main (String[] args) {
+    int t = 0;
+    while(t <= 50){
+      t++;
+      if(t %2 == 0){
+        System.out.println(t);
+      }
+    }
+  }
+}
