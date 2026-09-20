7c7d4e5975b5ecc63356245f37ab753db7145f41
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-20 14:06:34 +0900

    Break.java作成

---
+++ JavaSample/chapter6/Break.java
@@ -0,0 +1,8 @@
+public class Break {
+  public static void main (String[] args) {
+    int x = (int)(Math.random()*6+1), y = (int)(Math.random()*6+1);
+    System.out.println(x);
+    System.out.println(y);
+
+  }
+}
