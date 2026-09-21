568ac35095fd8f142ba031aac7d85f4d118b62ef
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-21 17:23:55 +0900

    Continue.java作成

---
+++ JavaSample/chapter6/Continue.java
@@ -0,0 +1,17 @@
+public class Continue {
+  public static void main (String[] args) {
+    for (;;) {
+      int a = (int)(Math.random()*6+1);
+      int b = (int)(Math.random()*6+1);
+
+      if(a>b) {
+        continue;
+      }
+      System.out.println(a +" " +b);
+
+      if(a == b) {
+        break;
+      }
+    }
+  }
+}
