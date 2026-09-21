55f0c5b75ecc4bf1843993a1dc839ecadda63e6c
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-21 22:17:28 +0900

    Fukushu6.java作成

--- JavaSample/chapter6/Fukushu6.java
+++ JavaSample/chapter6/Fukushu6.java
@@ -2,11 +2,15 @@ public class Fukushu6 {
   public static void main (String[] args) {
     for(;;){
     int a = (int)(Math.random()*6+1), b = (int)(Math.random()*6+1);
-    System.out.print(a+" ");
-    System.out.println(b);
-    if(a == b){
-      break;
+    if(a > b){
+      continue;
     }
+  System.out.print(a+" "+b);
+  if(a==b){
+    break;
+  }
+
+
     }
   }
 }
