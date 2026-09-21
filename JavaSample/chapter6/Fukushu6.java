daacd4867fd6948ff716e68ed15aa204d326e626
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-21 22:07:27 +0900

    Fukushu6.java作成

--- JavaSample/chapter6/Fukushu6.java
+++ JavaSample/chapter6/Fukushu6.java
@@ -1,8 +1,12 @@
 public class Fukushu6 {
   public static void main (String[] args) {
-    do {
-      System.out.println("Win!");
-    } while(Math.random()>0.5);
-    System.out.print("Lose");
+    for(;;){
+    int a = (int)(Math.random()*6+1), b = (int)(Math.random()*6+1);
+    System.out.print(a+" ");
+    System.out.println(b);
+    if(a == b){
+      break;
+    }
+    }
   }
 }
