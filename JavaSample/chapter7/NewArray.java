41a36b9b439c2ee111ad719f8dd92ebb72f479a3
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-22 18:53:27 +0900

    NewArray.java作成

--- JavaSample/chapter7/NewArray.java
+++ JavaSample/chapter7/NewArray.java
@@ -1,8 +1,8 @@
-public class Array3 {
+public class NewArray {
   public static void main (String[] args){
-    int[] price = {120,230,340,450,560,670,780,890};
-    for(int p: price){
-      System.out.print(p+" ");
+    int[] dice = new int[30];
+    for (int d: dice){
+      System.out.print(d+" ");
     }
   }
   
