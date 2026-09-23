baa951251b68ef6e612aa60756defc66643c352e
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-23 14:54:11 +0900

    Args3.java作成

--- JavaSample/chapter7/Args3.java
+++ JavaSample/chapter7/Args3.java
@@ -1,11 +1,14 @@
-public class Args2 {
+public class Args3 {
   public static void main (String[] args) {
-    int total = 0;
 
-    for(String a: args){
-      total += Integer.parseInt(a);
+    if(args.length == 0){
+      System.out.println("usage: java Args3 <integer>...");
+    } else {
+      int total = 0;
+      for(String a: args){
+        total += Integer.parseInt(a);
+      }
+      System.out.println(total);
     }
-    System.out.println(total);
   }
-  
 }
