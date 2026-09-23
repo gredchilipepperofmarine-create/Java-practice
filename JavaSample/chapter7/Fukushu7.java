bece7b3dc7dd75cef03c591fafd255e02476789c
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-23 21:11:37 +0900

    Fukushu7.java完了

--- JavaSample/chapter7/Fukushu7.java
+++ JavaSample/chapter7/Fukushu7.java
@@ -1,9 +1,14 @@
 public class Fukushu7 {
   public static void main (String[] args) {
-    int[][] table = new int[9][9];
+    int[][] table = new int[9][];
     for(int i=0; i<table.length; i++){
-      for(int j=0; j<table[i].length; j++){
+
+      table[i] = new int[i+1];
+
+      for(int j=0; j<i+1; j++){
+
         table[i][j] = (i+1)*(j+1);
+
       }
     }
     for(int[] row:table){
