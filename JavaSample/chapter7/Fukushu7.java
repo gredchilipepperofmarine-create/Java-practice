8411a568969a91ec0dfc88c7d4dbaa762290dc33
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-23 20:12:58 +0900

    Fukushu7.java作成

--- JavaSample/chapter7/Fukushu7.java
+++ JavaSample/chapter7/Fukushu7.java
@@ -1,17 +1,10 @@
 public class Fukushu7 {
   public static void main (String[] args) {
-    int[] dice = new int[30];
-    for(int i = 0; i<dice.length; i++){
-      dice[i] = (int)(Math.random()*6+1);
+    int total = 0;
+    for(String a:args){
+      total += Integer.parseInt(a);
     }
-    for(int j = 1; j < 7; j++){
-      for (int d:dice){
-        if(j == d){
-          System.out.print(d+" ");
-        }
-      }
-    }
-    System.out.println();
+    System.out.println(total);
   }
   
 }
