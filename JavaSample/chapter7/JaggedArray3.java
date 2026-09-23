973fb54ff793d2a350c8dc9b7900d830e141f2e1
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-23 19:37:01 +0900

    JaggedArray3.java作成

--- JavaSample/chapter7/JaggedArray3.java
+++ JavaSample/chapter7/JaggedArray3.java
@@ -1,8 +1,11 @@
-public class JaggedArray2 {
+public class JaggedArray3 {
   public static void main (String[] args) {
-    int[][] table = new int [9][9];
+    int[][] table = new int [9][];
 
     for(int i = 0; i < table.length; i++){
+
+      table[i] = new int[i+1];
+
       for(int j = 0; j < table[i].length; j++){
         table[i][j] = (i+1)*(j+1);
       }
@@ -10,7 +13,7 @@ public class JaggedArray2 {
 
     for(int[] row: table){
       for(int col: row){
-        System.out.print(col+" ");
+        System.out.printf("%2d ", col);
       }
       System.out.println();
     }
