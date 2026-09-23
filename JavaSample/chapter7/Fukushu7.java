c144b5520724cbfdc707f4f3c01bf04225728608
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-23 20:19:34 +0900

    Fukushu7.java作成

--- JavaSample/chapter7/Fukushu7.java
+++ JavaSample/chapter7/Fukushu7.java
@@ -1,10 +1,14 @@
 public class Fukushu7 {
   public static void main (String[] args) {
     int total = 0;
+    if(args.length == 0){
+      System.out.println("0dayo");
+    } else {
     for(String a:args){
       total += Integer.parseInt(a);
     }
     System.out.println(total);
+  }
   }
   
 }
