55aef49023de54e83fd534d7771d25d854e594c2
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-23 20:56:42 +0900

    Fukushu7.java作成

--- JavaSample/chapter7/Fukushu7.java
+++ JavaSample/chapter7/Fukushu7.java
@@ -1,13 +1,14 @@
 public class Fukushu7 {
   public static void main (String[] args) {
-    String[][] menu = {
-      {"bread","crape"},
-      {"cocoa","coffee","milk"},
-      {"apple", "orange"}
-    };
-    for(String[] category:menu){
-      for(String item: category){
-        System.out.print(item+" ");
+    int[][] table = new int[9][9];
+    for(int i=0; i<table.length; i++){
+      for(int j=0; j<table[i].length; j++){
+        table[i][j] = (i+1)*(j+1);
+      }
+    }
+    for(int[] row:table){
+      for(int col:row){
+        System.out.print(col+" ");
       }
       System.out.println();
     }
