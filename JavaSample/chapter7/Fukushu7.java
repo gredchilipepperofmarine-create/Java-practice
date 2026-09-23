4bd7c4d32603eac8ce86d7a95c3dbc1aa5331c8c
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-23 20:42:40 +0900

    Fukushu7.java作成

--- JavaSample/chapter7/Fukushu7.java
+++ JavaSample/chapter7/Fukushu7.java
@@ -1,6 +1,13 @@
 public class Fukushu7 {
   public static void main (String[] args) {
-    int[][] matrix = {{11,12,13},{21,22,23}};
+    int[][] matrix = new int[2][3];
+    for(int i = 0; i < matrix.length; i++){
+      for(int j = 0; j < matrix[i].length; j++){
+        matrix[i][j] = (i+1)*10+(j+1);
+      }
+    }
+
+
     for (int[] row: matrix){
       for(int col: row){
         System.out.print(col+" ");
