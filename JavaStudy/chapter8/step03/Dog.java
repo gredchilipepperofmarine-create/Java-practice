49e89820631a73a54eb1fcecc320a3cdd3c6ea01
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-24 19:17:30 +0900

    step03完了

---
+++ JavaStudy/chapter8/step03/Dog.java
@@ -0,0 +1,15 @@
+package JavaStudy.chapter8.step03;
+
+public class Dog {
+
+  // フィールドの宣言
+  String name;
+  int age;
+
+  // コンストラクタの宣言
+  Dog(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+}
