eaa6028c2b5181f3d7f8d5341df9ef14ee64d308
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-25 11:58:28 +0900

    step04作成

---
+++ JavaStudy/chapter8/step04/Dog.java
@@ -0,0 +1,18 @@
+package JavaStudy.chapter8.step04;
+
+public class Dog {
+  // フィールドの宣言
+  String name;
+  int age;
+
+  // コンストラクタの宣言
+  Dog(String name, int age){
+    this.name = name;
+    this.age = age;
+  }
+
+  // メソッドの宣言
+  String profile() {
+    return this.name + "(" + this.age + ")";
+  }
+}
