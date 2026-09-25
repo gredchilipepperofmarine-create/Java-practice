1967f7058cb462bc69bf78424fc3e467f33882b6
gredchilipepperofmarine-create <g.redchilipepper.ofmarine@gmail.com>
2026-09-25 21:40:06 +0900

    Fukushu8作成

--- JavaStudy/chapter8/Fukushu8/Dog.java
+++ JavaStudy/chapter8/Fukushu8/Dog.java
@@ -10,6 +10,24 @@ public class Dog {
     this.age = age;
   }
 
+  // nameのゲッター
+  public String getName() {
+    return this.name;
+  }
+  // nameのセッター
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  // ageのゲッター
+  public int getAge() {
+    return this.age;
+  }
+  // ageのセッター
+  public void setAge(int age) {
+    this.age = age;
+  }
+
   // メソッド
   public String profile () {
     return this.name + "(" + this.age + ")";
--- JavaStudy/chapter8/Fukushu8/Kennel.java
+++ JavaStudy/chapter8/Fukushu8/Kennel.java
@@ -2,10 +2,12 @@ public class Kennel {
   public static void main (String[] args) {
 
     Dog black = new Dog("Kuro", 2);
-    black.eat();
+    black.setAge(black.getAge()+1);
+    System.out.println(black.profile());
 
     Dog white = new Dog("Shiro", 3);
-    white.eat("cheese");
+    white.setName(white.getName().toUpperCase());
+    System.out.println(white.profile());
   }
   
 }
